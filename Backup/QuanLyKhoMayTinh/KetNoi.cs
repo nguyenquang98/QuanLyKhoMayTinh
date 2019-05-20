8a4d7b5af5a386ad92ab189bb8cfc49acb780e77
nguyenquang98 <nguyenquang24031998@gmail.com>
2019-05-20 19:35:57 +0700

    quang

---
+++ Backup/QuanLyKhoMayTinh/KetNoi.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace QuanLyKhoMayTinh
+{
+    class KetNoi
+    {
+        public SqlConnection taoketnoi()
+        {
+            return new SqlConnection(@"server = HP-91149BFA9401\SQLEXPRESS;uid = admin;pwd=admin");
+        }
+        public DataTable hienthi(string sql)
+        {
+            SqlConnection sqlConn = taoketnoi();
+            DataTable dt = new DataTable();
+            SqlDataAdapter sqlDa = new SqlDataAdapter(sql, sqlConn);
+            sqlDa.Fill(dt);
+            return dt;
+        }
+        public void capnhat(string sql)
+        {
+            SqlConnection sqlConn = taoketnoi();
+            sqlConn.Open();
+            SqlCommand sqlComm = new SqlCommand(sql, sqlConn);
+            sqlComm.ExecuteNonQuery();
+            sqlComm.Dispose();
+            sqlConn.Close();
+        }
+     
+        
+        
+        
+    }
+}
---
+++ QuanLyKhoMayTinh/frmBaoCaoKhachHang.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using CrystalDecisions.CrystalReports.Engine;
+
+namespace QuanLyKhoMayTinh
+{
+    public partial class frmBaoCaoKhachHang : Form
+    {
+        public frmBaoCaoKhachHang()
+        {
+            InitializeComponent();
+        }
+        
+        private void frmBaoCaoKhachHang_Load(object sender, EventArgs e)
+        {
+            string sql = "SELECT * FROM KHACHHANG";
+            SqlConnection sqlConn = new SqlConnection(@"Data Source=DESKTOP-6IA4IHN;Initial Catalog=khomaytinh;Integrated Security=True");
+            SqlDataAdapter sqlDa = new SqlDataAdapter(sql,sqlConn);
+            DataTable dt = new DataTable();
+            sqlDa.Fill(dt);
+            rptKhachHang cr = new rptKhachHang();
+            cr.SetDataSource(dt);
+            rptKhachHang.ReportSource = cr;
+            
+        }
+
+        
+    }
+}
